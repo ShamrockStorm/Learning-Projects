774f036e55264512b3cdc29e30818665c3c9ebe0
Daniel <blaga.daniel29@gmail.com>
2022-04-14 17:54:31 +0300

    Kruskal's algorithm first impl

--- Java/dataservice/src/main/java/com/licence/dataservice/locationhandleing/DeliveryRouteCalculator.java
+++ Java/dataservice/src/main/java/com/licence/dataservice/locationhandleing/DeliveryRouteCalculator.java
@@ -9,13 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DeliveryRouteCalculator {
+    List<Location> locations;
     public List<Route> GetRoutesForDelivery(Integer deliveryId){
         List<Route> routes = new ArrayList();
         return routes;
     }
     public List<Route> GetTestRoutes() throws IOException {
         DirectionsController dir = new DirectionsController();
-        List<Location> locations = new ArrayList<>();
+        locations = new ArrayList<>();
         Location warehouse = new Location(44.877526,24.854863); // Strada Depozitelor
         Location loc1 = new Location(44.864840,24.862019); //In nord, spitalul militar
         Location loc2 = new Location(44.863562,24.875752); // Calea Bucuresti
@@ -39,9 +40,60 @@ public class DeliveryRouteCalculator {
     public void GetDeliveryRoute() throws IOException {
         List<Route> deliveryRoute = new ArrayList<>();
         List<Route> routes = GetTestRoutes();
+        routes.sort(Route::compareTo);
+        int noDestinations = locations.size();
+        double cost = 0.0;
+        int[] reached = new int[noDestinations];
+        double[] minCost = new double[noDestinations-1];
+        int[] root = new int[noDestinations-1];
+        reached[0] = 1;
+        for(int i = 1; i<noDestinations-1;i++){
+            reached[i] = 0;
+            for (Route r : routes) {
+                if(r.getOrigin().equals(locations.get(0)) && r.getDestination().equals(locations.get(i)))
+                    minCost[i] = r.getLength();
+            }
+            root[i] = 1;
+        }
+        int y=0;
+        int x=0;
+        for(int l = 0; l < noDestinations-1; l++){
+            double min = 500.0;
+            for(int i = 1; i< noDestinations-1;i++){
+                if(reached[i] == 0 && minCost[i] < min){
+                    min = minCost[i];
+                    y = i;
+                }
+            }
+            reached[y] = 1;
+            x=root[y];
+            for (Route r : routes) {
+                if(r.getOrigin().equals(locations.get(x)) && r.getDestination().equals(locations.get(y)))
+                    cost = cost + r.getLength();
+            }
+            for(int i = 1; i< noDestinations-1;i++){
+                double curentCost = 0.0;
+                for (Route r : routes) {
+                    if((r.getOrigin().equals(locations.get(i)) && r.getDestination().equals(locations.get(y)))
+                        || (r.getOrigin().equals(locations.get(y)) && r.getDestination().equals(locations.get(i)))) {
+                        curentCost = r.getLength();
+                    }
+
+                }
+                if(reached[i] == 0 && minCost[i] > curentCost){
+                    minCost[i] = curentCost;
+                    root[i] = y;
+                }
+            }
+        }
+        for (int i : root){
+            System.out.println(i);
+        }
+        /*
         for (Route r : routes) {
             System.out.println(r.toString());
         }
+         */
         //return deliveryRoute;
     }
 }
--- Java/dataservice/src/main/java/com/licence/dataservice/locationhandleing/route/Route.java
+++ Java/dataservice/src/main/java/com/licence/dataservice/locationhandleing/route/Route.java
@@ -2,10 +2,20 @@ package com.licence.dataservice.locationhandleing.route;
 
 import com.licence.dataservice.locationhandleing.Location;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 @Data
-public class Route {
+public class Route implements Comparable<Route> {
     Location origin;
     Location destination;
     Double length;
+
+    @Override
+    public int compareTo(@NotNull Route route) {
+        if(this.length == route.getLength()) return 0;
+        else{
+            if(this.length > route.getLength()) return 1;
+            else return -1;
+        }
+    }
 }
